b493dd8566a5b25a784e20ecaa329830ee2c1350
DESKTOP-VP70K2A\Victor <victor.poleyko@gmail.com>
2018-04-20 21:20:41 +0300

    +Email message model added

---
+++ Rocket.BL.Common/Models/EmailMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket.BL.Common.Models
+{
+    class EmailMessage
+    {
+        public string ReceiverEmailAddress { get; set; }
+
+        public string SenderEmailAddress { get; set; }
+
+        public string SenderEmailPassword { get; set; }
+        
+        public string MessageBody { get; set; }
+
+        public string MessageTheme { get; set; }
+    }
+}
