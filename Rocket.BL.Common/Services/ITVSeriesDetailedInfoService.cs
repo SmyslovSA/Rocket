18b99ab0acf6fbd56c09dd40724bdd9d283b6a42
Mikalai Kharevich <mikalaikharevich@gmail.com>
2018-04-22 00:50:36 +0300

    ROC-78, ROC-91 Создание интерфейсов сервисов детальной информации о фильмах и сериалах + добавлен интерфейс для работы с детальной информацией о фильмах + добавлен интерфейс для работы с детальной информацией о сериалах - удалена заглушка IService.cs

---
+++ Rocket.BL.Common/Services/ITVSeriesDetailedInfoService.cs
@@ -0,0 +1,51 @@
+﻿using Rocket.BL.Common.Models.ReleaseList;
+
+namespace Rocket.BL.Common.Services
+{
+    /// <summary>
+    /// Представляет сервис для работы с детальной информацией
+    /// о сериалах в хранилище данных
+    /// </summary>
+    interface ITVSeriesDetailedInfoService
+    {
+        /// <summary>
+        /// Возвращает сериал с заданным идентификатором из хранилища данных
+        /// </summary>
+        /// <param name="id">Идентификатор сериала</param>
+        /// <returns>Экземпляр сериала</returns>
+        TVSeries GetTVSeries(int id);
+
+        /// <summary>
+        /// Добавляет заданный сериал в хранилище данных
+        /// и возвращает идентификатор добавленного сериала.
+        /// Если добавление сериала завершилось неудачей,
+        /// возвращает -1.
+        /// </summary>
+        /// <param name="tvSeries">Экземпляр сериала для добавления</param>
+        /// <returns>Идентификатор сериала</returns>
+        int AddTVSeries(TVSeries tvSeries);
+
+        /// <summary>
+        /// Обновляет информацию заданного сериала в хранилище данных
+        /// и возвращает значение, которое определяет
+        /// прошла ли операция успешно
+        /// </summary>
+        /// <param name="tvSeries">Экземпляр сериала для обновления</param>
+        /// <returns>Возвращает <see langword="true"/> при успешном обновлении</returns>
+        bool UpdateTVSeries(TVSeries tvSeries);
+
+        /// <summary>
+        /// Удаляет сериал с заданным идентификатором из хранилища данных.
+        /// </summary>
+        /// <param name="id">Идентификатор сериала</param>
+        /// <returns>Возвращает <see langword="true"/> при успешном удалении</returns>
+        bool DeleteTVSeries(int id);
+
+        /// <summary>
+        /// Проверяет наличие заданного сериала в хранилище данных
+        /// </summary>
+        /// <param name="tvSeries">Экземпляр сериала для проверки</param>
+        /// <returns>Возвращает <see langword="true"/>, если сериал существует в хранилище данных</returns>
+        bool TVSeriesExists(TVSeries tvSeries);
+    }
+}
