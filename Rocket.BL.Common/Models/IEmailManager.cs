3317230f903f1841c2292ae36b645a80db4f7dd1
SmyslovSA <stepan.smyslov@gmail.com>
2018-04-21 13:37:59 +0300

    +add user model + add personal cabinet interfaces

---
+++ Rocket.BL.Common/Models/IEmailManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket.BL.Common.Models
+{
+    interface IEmailManager
+    {
+        bool AddEmail(SimpleUser user, string email);
+        bool DeleteEmail(SimpleUser user, string email);
+    }
+}
---
+++ Rocket.BL.Common/Models/IPersonalData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket.BL.Common.Models
+{
+    interface IPersonalData
+    {
+        bool ChangePersonalData(SimpleUser user, string firstName, string lastName, string avatar);
+        bool ChangePasswordData(SimpleUser user, string newPassword, string newPasswordConfirm);
+    }
+}
