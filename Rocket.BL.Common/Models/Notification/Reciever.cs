29672a3fdb15a1f3092eb43313292b8d5563eb5a
Ilya Dvortsevoy <links309@gmail.com>
2018-04-23 00:27:03 +0300

    + Reciever model created

--- Rocket.BL.Common/Models/Notification/Reciever.cs
+++ Rocket.BL.Common/Models/Notification/Reciever.cs
@@ -6,5 +6,21 @@ using System.Threading.Tasks;
 
 namespace Rocket.BL.Common.Models.Notification
 {
-    
+    /// <summary>
+    /// Describes user or guest which
+    /// is the reciever of message
+    /// </summary>
+    class Reciever
+    {
+        /// <summary>
+        /// Gets or sets the current user or guest name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current user or guest
+        /// email address
+        /// </summary>
+        public string EmailAddress { get; set; }
+    }
 }
