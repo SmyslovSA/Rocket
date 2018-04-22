cbd9df28f771a989ca630b175ad0a4da94172895
Ilya Dvortsevoy <links309@gmail.com>
2018-04-23 00:34:04 +0300

    + Sender model created

---
+++ Rocket.BL.Common/Models/Notification/Sender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket.BL.Common.Models.Notification
+{
+    /// <summary>
+    /// Describes administrator or system
+    /// which sends the message
+    /// </summary>
+    class Sender
+    {
+        /// <summary>
+        /// Gets or sets the current administrator
+        /// or system name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current administrator
+        /// or system email address
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password of the current
+        /// administrator or system account
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
