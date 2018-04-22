9dbcdd67c63182eb660d247da53c9ed7eb842e22
Mikalai Kharevich <mikalaikharevich@gmail.com>
2018-04-22 12:53:29 +0300

    ROC-95, ROC-96 Создание моделей хранения данных фильмов и сериалов + добавлена модель хранения данных фильма + добавлена модель хранения данных сериала + добавлена модель хранения данных сезона + добавлена модель хранения данных серии + добавлена модель хранения данных жанра видео + добавлена модель хранения данных страны + добавлена модель хранения данных человека (актёра, режиссёра)

---
+++ Rocket.DAL.Common/DbModels/DbPerson.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Rocket.DAL.Common.DbModels
+{
+    /// <summary>
+    /// Представляет модель хранения данных о человеке (режиссере, актёре или музыканте)
+    /// </summary>
+    public class DbPerson
+    {
+        /// <summary>
+        /// Возвращает или задает уникальный идентификатор человека
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает имя человека
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает фамилию человека
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию фильмов,
+        /// в которых этот человек был режиссером
+        /// </summary>
+        public ICollection<DbFilm> DbFilmsDirector { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию фильмов,
+        /// в которых этот человек был актёром
+        /// </summary>
+        public ICollection<DbFilm> DbFilmsActor { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию сериалов,
+        /// в которых этот человек был режиссером
+        /// </summary>
+        public ICollection<DbTVSeries> DbTVSerialsDirector { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию сериалов,
+        /// в которых этот человек был актёром
+        /// </summary>
+        public ICollection<DbTVSeries> DbTVSerialsActor { get; set; }
+    }
+}
---
+++ Rocket.DAL.Common/DbModels/DbSeason.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Rocket.DAL.Common.DbModels
+{
+    /// <summary>
+    /// Представляет модель хранения данных о сезонах сериалов
+    /// </summary>
+    public class DbSeason
+    {
+        /// <summary>
+        /// Возвращает или задает уникальный идентификатор сезона
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает порядковый номер сезона
+        /// </summary>
+        public int Number { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает путь к изображению постера сезона
+        /// </summary>
+        public string PosterImagePath { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает краткое описание сезона
+        /// </summary>
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию серий сезона
+        /// </summary>
+        public ICollection<DbEpisode> Episodes { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает идентификатор сериала,
+        /// к которому относится этот сезон
+        /// </summary>
+        public int TVSeriesId { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает сериал,
+        /// к которому относится этот сезон
+        /// </summary>
+        public DbTVSeries TVSeries { get; set; }
+    }
+}
