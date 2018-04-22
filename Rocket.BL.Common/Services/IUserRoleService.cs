c6a4e68b9906b91a5984ca4b1b0de1b328a7007a
medpavol <medpavol@gmail.com>
2018-04-22 19:23:28 +0300

    test

---
+++ Rocket.BL.Common/Services/IUserRoleService.cs
@@ -0,0 +1,30 @@
+﻿using Rocket.DAL.Common.Repositories;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket.BL.Common.Services
+{
+    public interface IUserRoleService
+    {
+        /// <summary>
+        /// Add new UserRole. return success/failed 
+        /// </summary>
+        /// <returns></returns>
+        bool AddRole(ushort roleId, string roleName);
+
+        /// <summary>
+        /// UserRole.IsActive - switch true/false 
+        /// </summary>
+        void SwitchRoleVisibility(ushort roleId);
+
+        /// <summary>
+        /// get totally list
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable FetchAll();
+    }
+}
