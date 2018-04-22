08d7f0b477318ae2ba0f6fe2324e007f5c376b46
medpavol <medpavol@gmail.com>
2018-04-22 19:34:06 +0300

    снова тест

--- Rocket.BL.Common/Services/IRolePermissionService.cs
+++ Rocket.BL.Common/Services/IRolePermissionService.cs
@@ -1,27 +1,14 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Rocket.DAL.Common.Repositories;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.DAL.Common.Repositories;
 
-//namespace Rocket.BL.Common.Services
-//{
-//    public interface IRolePermissionService
-//    {
-//        /// <summary>
-//        /// try Add new RolePermission. return success/failed 
-//        /// </summary>
-//        /// <returns></returns>
-//        bool AddPermission(ushort key, string value);
+namespace Rocket.BL.Common.Services
+{
+    public interface IRolePermissionService
+    {
 
-//        /// <summary>
-//        /// try remove some permission by key
-//        /// </summary>
-//        /// <returns></returns>
-//        bool RemovePermission(ushort key);
-
-//        // get totally list
-//        // FetchAll();
-//    }
-//}
+    }
+}
