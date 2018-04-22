bf163271e85e7477d75f5fc4f2238760b57f01aa
Ilya Dvortsevoy <links309@gmail.com>
2018-04-23 00:17:51 +0300

    Merge branch 'develop' of https://gitlab.belpyro.net/pvt-q1-2018/rocket into feature/ROC-68_Уведомления_по_e-mail

---
+++ Rocket.BL.Common/Models/ReleaseList/Episode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Rocket.BL.Common.Models.ReleaseList
+{
+    /// <summary>
+    /// Представляет информацию о конкретной серии сериала
+    /// </summary>
+    public class Episode
+    {
+        /// <summary>
+        /// Возвращает или задает уникальный идентификатор серии
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает информацию о выходе серии
+        /// </summary>
+        public Release Release { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает номер серии в сезоне
+        /// </summary>
+        public int Number { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает название серии
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает продолжительность серии
+        /// </summary>
+        public TimeSpan Duration { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает краткое описание серии
+        /// </summary>
+        public string Summary { get; set; }
+    }
+}
---
+++ Rocket.BL.Common/Models/ReleaseList/TVSeries.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace Rocket.BL.Common.Models.ReleaseList
+{
+    /// <summary>
+    /// Представляет информацию о конкретном сериале
+    /// </summary>
+    public class TVSeries
+    {
+        /// <summary>
+        /// Возвращает или задает уникальный идентификатор сериала
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает название сериала
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает путь к изображению постера сериала
+        /// </summary>
+        public string PosterImagePath { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию режиссеров, которые сняли сериал
+        /// </summary>
+        public ICollection<Person> Directors { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию актёров, которые снялись в сериале
+        /// </summary>
+        public ICollection<Person> Cast { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию жанров, к которым относится сериал
+        /// </summary>
+        public ICollection<VideoGenre> Genres { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию стран, которые участвовали в создании сериала
+        /// </summary>
+        public ICollection<string> Countries { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает краткое описание сериала
+        /// </summary>
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию сезонов сериала
+        /// </summary>
+        public ICollection<Season> Seasons { get; set; }
+    }
+}
