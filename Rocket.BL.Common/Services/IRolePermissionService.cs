6e93cf0ee86506e7d55ea43c68170c4d35572e8c
medpavol <medpavol@gmail.com>
2018-04-22 19:26:03 +0300

    test2

---
+++ Rocket.BL.Common/Services/IRolePermissionService.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Rocket.DAL.Common.Repositories;
+
+//namespace Rocket.BL.Common.Services
+//{
+//    public interface IRolePermissionService
+//    {
+//        /// <summary>
+//        /// try Add new RolePermission. return success/failed 
+//        /// </summary>
+//        /// <returns></returns>
+//        bool AddPermission(ushort key, string value);
+
+//        /// <summary>
+//        /// try remove some permission by key
+//        /// </summary>
+//        /// <returns></returns>
+//        bool RemovePermission(ushort key);
+
+//        // get totally list
+//        // FetchAll();
+//    }
+//}
