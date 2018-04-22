7cdaffe471d89f645ae20c468c249fedc2aec7eb
medpavol <medpavol@gmail.com>
2018-04-22 12:21:09 +0300

    + interface IRepository implemented

--- Rocket.DAL.Common/Repositories/IRepository.cs
+++ Rocket.DAL.Common/Repositories/IRepository.cs
@@ -1,7 +1,40 @@
-﻿namespace Rocket.DAL.Common.Repositories
-{
-  public interface IRepository
-  {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-  }
+namespace Rocket.DAL.Common.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class, IDisposable
+    {
+        /// <summary>
+        /// get list of entity
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<TEntity> FetchAll();
+
+        /// <summary>
+        /// return TEntity by key
+        /// </summary>
+        TEntity GetElementById(int key);
+
+        /// <summary>
+        /// standart operation Add
+        /// </summary>
+        /// <param name="entity"></param>
+        void Add(TEntity entity);
+
+        /// <summary>
+        /// standart operation Update
+        /// </summary>
+        /// <param name="entity"></param>
+        void Update(TEntity entity);
+
+        /// <summary>
+        /// standart operation Delete
+        /// </summary>
+        /// <param name="entity"></param>
+        void Delete(TEntity entity);
+    }
 }
+
+
