997dfec5b58f69633e957749d8c0ee895955c638
SmyslovSA <stepan.smyslov@gmail.com>
2018-04-23 03:10:17 +0300

    * PersonalizedTape change dictionary parameters

---
+++ Rocket.BL.Common/Models/PersonalArea/PersonalizedTape.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Rocket.BL.Common.Enums;
+
+namespace Rocket.BL.Common.Models
+{
+    public class PersonalizedTape
+    {
+        /// <summary>
+        /// персональный список релизов в виде: категория продукта - список жанров продукта
+        /// </summary>
+        public IDictionary<Categories, ICollection<Genres>> Categories { get; set; }    
+    }
+}
