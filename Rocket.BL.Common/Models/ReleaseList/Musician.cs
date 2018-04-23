03d56da6bd18e2e3bc68d77ac2fa2483c900cfcb
Artem Akhuba <artemakhyba@gmail.com>
2018-04-23 09:53:18 +0300

    + add ROC-77 Создание модели музыки, ROC-92 Создание интерфейса сервиса детальной информации о музыке, ROC-99 Создание моделей хранения данных для музыки

---
+++ Rocket.BL.Common/Models/ReleaseList/Musician.cs
@@ -0,0 +1,26 @@
+﻿namespace Rocket.BL.Common.Models.ReleaseList
+{
+	public class Musician
+	{
+		/// <summary>
+		/// Возвращает или задает уникальный идентификатор музыканта
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Возвращает или задает имя музыканта
+		/// </summary>
+		public string FirstName { get; set; }
+
+		/// <summary>
+		/// Возвращает или задает фамилию музыканта
+		/// </summary>
+		public string LastName { get; set; }
+
+		/// <summary>
+		/// Возвращает или задает псевдоним музыканта
+		/// </summary>
+		public string Alias { get; set; }
+	}
+
+}
