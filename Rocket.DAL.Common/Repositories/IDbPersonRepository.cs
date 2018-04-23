37a9cc723765f051bccf54bf86c87a0b62364003
Mikalai Kharevich <mikalaikharevich@gmail.com>
2018-04-23 14:54:22 +0300

    ROC-97, ROC-98 - Создание интерфейсов репозиториев для работы с фильмами и сериалами * изменена заглушка IRepository на обобщенный интерфейс * изменена заглушка IUnitOfWork на общий интерфейс + добавлены интерфейсы репозиториев для моделей связанных с фильмами и сериалами + добавлены интерфейсы unit of work для работы с фильмами и сериалами

---
+++ Rocket.DAL.Common/Repositories/IDbPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿using Rocket.DAL.Common.DbModels;
+
+namespace Rocket.DAL.Common.Repositories
+{
+    /// <summary>
+    /// Представляет репозиторий для людей (актёров, режиссёров)
+    /// </summary>
+    public interface IDbPersonRepository :IRepository<DbPerson>
+    {
+    }
+}
