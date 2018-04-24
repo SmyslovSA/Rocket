f858e8caa966192daa369fbb65425a43ef6476fe
sychounm <sychounm@gmail.com>
2018-04-24 22:59:57 +0300

    add sheduller add lostfilm parser beginner version

---
+++ Rocket.Parser/Service.cs
@@ -0,0 +1,17 @@
+﻿using Topshelf;
+
+namespace Rocket.Parser
+{
+    public class Service : ServiceControl
+    {
+        public bool Start(HostControl hostControl)
+        {
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            return true;
+        }
+    }
+}
