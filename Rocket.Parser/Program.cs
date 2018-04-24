035b94d5ff22d939f657305467d4cfd874234767
Denis Susha <lumen446@gmail.com>
2018-04-25 02:15:16 +0300

    * upd parser program

--- Rocket.Parser/Program.cs
+++ Rocket.Parser/Program.cs
@@ -33,54 +33,10 @@ namespace Rocket.Parser
                 }
                 catch (Exception e)
                 {
+                    // todo log
                     throw;
                 }
             });
-
-            //    var lostfilmParseService = new LostfilmParseService();
-
-            //    var test = Task.Run(() => lostfilmParseService.Parse()).Result;
-
-            //    Console.WriteLine("Use commands:");
-            //    Console.WriteLine("/start -- start parsing");
-            //    Console.WriteLine("/abort -- abort parsing");
-            //    Console.WriteLine("/quit -- close the app");
-
-            //    var quitNow = false;
-            //    while (!quitNow)
-            //    {
-            //        var parser = new Middleware();
-
-            //        var command = Console.ReadLine();
-            //        switch (command)
-            //        {
-            //            case "/start":
-            //                Console.Write("Write start page:");
-            //                var startPage = Convert.ToInt32(Console.ReadLine());
-
-            //                Console.Write("Write end page:");
-            //                var endPage = Convert.ToInt32(Console.ReadLine());
-
-            //                parser.Start(startPage, endPage);
-
-            //                break;
-
-            //            case "/abort":
-            //                parser.Abort();
-            //                Console.WriteLine("App was abort");
-            //                break;
-
-            //            case "/quit":
-            //                parser.Abort();
-            //                quitNow = true;
-            //                break;
-
-            //            default:
-            //                Console.WriteLine("Unknown Command " + command);
-            //                break;
-            //        }
-            //    }
-            //}
         }
 
         /// <summary>
