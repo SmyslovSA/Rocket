22c0c5b8958877059ae3a6e6f59cf8be05c3cd8e
medpavol <medpavol@gmail.com>
2018-04-26 18:51:14 +0300

    * добавлен проп в модели Permission + инициация DbConfigurations

---
+++ Rocket.DAL/Configurations/UserRoleEntities/DbRoleConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Rocket.DAL.Common.DbModels.DbUserRole;
+
+namespace Rocket.DAL.Configurations.UserRoleEntities
+{
+    public class DbRoleConfiguration : EntityTypeConfiguration<DbRole>
+    {
+        public DbRoleConfiguration()
+        {
+            ToTable("t_user_role")
+                .HasKey(t => t.Id)
+                .Property(t => t.Id)
+                .HasColumnName("role_id");
+        }
+
+    }
+}
