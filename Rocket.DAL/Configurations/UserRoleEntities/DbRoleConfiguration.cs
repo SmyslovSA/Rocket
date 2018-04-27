f62be6b4ae0d3098593b57b3af0b4e86ea39d2cc
medpavol <medpavol@gmail.com>
2018-04-27 14:30:16 +0300

    * EF configurations Role, Permission * changed folder name "%UserRole%" * fixed interface to public

--- Rocket.DAL/Configurations/UserRoleEntities/DbRoleConfiguration.cs
+++ Rocket.DAL/Configurations/UserRoleEntities/DbRoleConfiguration.cs
@@ -11,7 +11,19 @@ namespace Rocket.DAL.Configurations.UserRoleEntities
                 .HasKey(t => t.Id)
                 .Property(t => t.Id)
                 .HasColumnName("role_id");
-        }
 
+            Property(t => t.Name)
+                .IsRequired()
+                .HasColumnName("name")
+                .HasMaxLength(50)
+                .IsVariableLength();
+
+            Property(t => t.IsActive)
+                .IsRequired()
+                .HasColumnName("is_active");
+
+            HasMany(t => t.Permissions)
+                .WithMany(x => x.Roles);
+        }
     }
 }
