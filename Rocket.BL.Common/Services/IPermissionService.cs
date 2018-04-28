3fa2144babc2f2a9fb1d0cf3496173de73f4ac31
medpavol <medpavol@gmail.com>
2018-04-28 13:40:32 +0300

    * солид наименования

--- Rocket.BL.Common/Services/IPermissionService.cs
+++ Rocket.BL.Common/Services/IPermissionService.cs
@@ -6,12 +6,12 @@
         /// Добавить существующую функц возможность для выбранной роли
         /// </summary>
         /// <returns></returns>
-        bool AddRoleToUserRoleList(ushort roleId, ushort permissionId);
+        bool AddPermissionToRole(ushort roleId, ushort permissionId);
 
         /// <summary>
         /// Удалить функц возможность из текущего списка у роли
         /// </summary>
         /// <returns></returns>
-        bool RemoveRoleFromUserRoleList(ushort roleId, ushort permissionId);
+        bool RemovePermissionFromRole(ushort roleId, ushort permissionId);
     }
 }
--- Rocket.DAL/Configurations/UserRoleEntities/DbPermissionConfiguration.cs
+++ Rocket.DAL/Configurations/UserRoleEntities/DbPermissionConfiguration.cs
@@ -12,7 +12,7 @@ namespace Rocket.DAL.Configurations.UserRoleEntities
             ToTable("t_user_permission")
                 .HasKey(t => t.Id)
                 .Property(t => t.Id)
-                .HasColumnName("prm_id");
+                .HasColumnName("permission_id");
 
             Property(t => t.Description)
                 .IsOptional()
@@ -22,7 +22,7 @@ namespace Rocket.DAL.Configurations.UserRoleEntities
 
             Property(t => t.ValueName)
                 .IsRequired()
-                .HasColumnName("value_name")
+                .HasColumnName("valueName")
                 .HasMaxLength(50)
                 .IsVariableLength();
 
