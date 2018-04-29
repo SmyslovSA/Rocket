95d336ce3402e68982a83e25f5478f00820e1d28
Vlad\vd-bar <vd-bar@wp.pl>
2018-04-29 10:30:14 +0300

    Add Configurations

---
+++ Rocket.DAL/Configurations/DbEmailConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Rocket.DAL.Common.DbModels.DbPersonalArea;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Rocket.DAL.Configurations
+{
+    class DbEmailConfiguration : EntityTypeConfiguration<DbEmail>
+    {
+        public DbEmailConfiguration()
+        {
+                 ToTable("Email")
+                .HasKey(e => e.Id)
+                .Property(f => f.Id)
+                .HasColumnName("Id");
+
+                  Property(f => f.Name)
+                 .IsRequired()
+                 .HasColumnName("Name");
+
+           
+        }
+
+
+        
+
+    }
+}
---
+++ Rocket.DAL/Configurations/DbPersonalizedTapeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Rocket.DAL.Common.DbModels.DbPersonalArea;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Rocket.DAL.Configurations
+{
+    class DbPersonalizedTapeConfiguration :EntityTypeConfiguration<DbPersonalizedTape>
+    {
+        public DbPersonalizedTapeConfiguration()
+        {
+            ToTable("PersonalizedTape")
+                .HasKey(t => t.Id)
+                .Property(t => t.Id)
+                .HasColumnName("Id");
+         
+
+        }
+    }
+}
