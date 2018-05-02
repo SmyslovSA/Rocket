c94c033da03ffc533fd33f154d761c14d08125b5
Ilya Dvortsevoy <links309@gmail.com>
2018-05-02 14:23:07 +0300

    + FilmForNotification domain model added

--- Rocket.BL.Common/Models/Notification/Receiver.cs
+++ Rocket.BL.Common/Models/Notification/Receiver.cs
@@ -1,26 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rocket.BL.Common.Models.Notification
+﻿namespace Rocket.BL.Common.Models.Notification
 {
     /// <summary>
-    /// Описывает гостя либо пользователя,
+    /// Описывает гостя либо пользователя, 
     /// являющихся получателями сообщения
     /// </summary>
     public class Receiver
     {
         /// <summary>
-        /// Возвращает либо задает имя гостя
-        /// или пользователя
+        /// Возвращает либо задает имя гостя или пользователя
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// Возвращает либо задает email
-        /// адрес гостя или пользователя
+        /// Возвращает либо задает email адрес гостя или пользователя
         /// </summary>
         public string Email { get; set; }
     }
