4df430ad4ee986f6d2784af34469430d9c1fa6eb
Ilya Dvortsevoy <links309@gmail.com>
2018-05-02 14:37:49 +0300

    + ConfirmationMessage domain model added

---
+++ Rocket.BL.Common/Models/Notification/ConfirmationMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace Rocket.BL.Common.Models.Notification
+{
+    /// <summary>
+    /// Описывает письмо со ссылкой для завершения
+    /// регистрации аккаунта пользователя
+    /// </summary>
+    public class ConfirmationMessage
+    {
+        /// <summary>
+        /// Задает или возвращает получателя сообщения
+        /// </summary>
+        public Receiver Receiver { get; set; }
+
+        /// <summary>
+        /// Задает или возвращает ссылку для завершения регистрации
+        /// </summary>
+        public string Link { get; set; }
+    }
+}
