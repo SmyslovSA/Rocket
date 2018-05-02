d2f007949d6d8ceefddae64902386eb7f22e71a4
Ilya Dvortsevoy <links309@gmail.com>
2018-05-02 14:45:15 +0300

    + BillingMessage domain model added

---
+++ Rocket.BL.Common/Models/Notification/BillingMessage.cs
@@ -0,0 +1,20 @@
+﻿namespace Rocket.BL.Common.Models.Notification
+{
+    /// <summary>
+    /// Описывает письмо с данными о совершенных
+    /// пользователем либо гостем платежах на сайте
+    /// (покупка премиум аккаунта, донат)
+    /// </summary>
+    public class BillingMessage
+    {
+        /// <summary>
+        /// Задает или возвращает получателя сообщения
+        /// </summary>
+        public Receiver Receiver { get; set; }
+
+        /// <summary>
+        /// Задает или возвращает оплаченную сумму
+        /// </summary>
+        public decimal Sum { get; set; }
+    }
+}
