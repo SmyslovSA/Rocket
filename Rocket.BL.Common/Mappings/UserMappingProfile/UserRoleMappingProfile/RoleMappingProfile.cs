898008bcddc9de643e705d897bbc6344bce882b5
medpavol <medpavol@gmail.com>
2018-05-04 13:46:35 +0300

    + маппинги для моделей * убраны лишние пробелы в моделях * удален пустой класс, без ссылки

---
+++ Rocket.BL.Common/Mappings/UserMappingProfile/UserRoleMappingProfile/RoleMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Rocket.BL.Common.Models.UserRoles;
+using Rocket.DAL.Common.DbModels.DbUserRole;
+
+namespace Rocket.BL.Common.Mappings.UserMappingProfile.UserRoleMappingProfile
+{
+    public class RoleMappingProfile : Profile
+    {
+        public RoleMappingProfile()
+        {
+            CreateMap<Role, DbRole>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.RoleId))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive))
+                .ForMember(dest => dest.Permissions, opt => opt.MapFrom(src => src.Permissions))
+                .ReverseMap();
+        }
+    }
+}
