5872e79aba075b3b19ee91d4299a4804ad08131c
Mikalai Kharevich <mikalaikharevich@gmail.com>
2018-05-06 00:59:40 +0300

    + Создана реализация базового обобщенного репозитория + Создана реализация UoF

--- Rocket.DAL/Context/RocketContext.cs
+++ Rocket.DAL/Context/RocketContext.cs
@@ -1,9 +1,48 @@
-﻿using System.Data.Entity;
+﻿using Rocket.DAL.Common.DbModels.ReleaseList;
+using Rocket.DAL.Configurations.ReleaseList;
+using System.Data.Entity;
 
 namespace Rocket.DAL.Context
 {
+    /// <summary>
+    /// Представляет контекст данных приложения
+    /// </summary>
     public class RocketContext : DbContext
     {
+        /// <summary>
+        /// Создает новый экземпляр контекста данных
+        /// </summary>
+        public RocketContext(string connectionString)
+            : base(connectionString)
+        {
+            Database.SetInitializer<RocketContext>(null);
+        }
 
+        /// <summary>
+        /// Возвращает или задает DbSet фильмов
+        /// </summary>
+        public IDbSet<DbFilm> Films { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает DbSet сериалов
+        /// </summary>
+        public IDbSet<DbTVSeries> TVSerials { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает DbSet музыки
+        /// </summary>
+        public IDbSet<DbMusic> Musics { get; set; }
+
+        /// <summary>
+        /// Этот метод вызывается, когда модель для производного контекста данных была инициализирована,
+        /// но до того, как модель была заблокирована и использована для инициализации этого контекста.
+        /// </summary>
+        /// <param name="modelBuilder">Построитель, который определяет модель для создаваемого контекста.</param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new DbFilmConfiguration());
+            modelBuilder.Configurations.Add(new DbTVSeriesConfiguration());
+            modelBuilder.Configurations.Add(new DbMusicConfiguration());
+        }
     }
 }
---
+++ Rocket.DAL/UoW/UnitOfWork.cs
@@ -0,0 +1,110 @@
+﻿using Rocket.DAL.Common.Repositories.ReleaseList;
+using Rocket.DAL.Common.UoW;
+using Rocket.DAL.Repositories.ReleaseList;
+using System;
+using System.Data.Entity;
+
+namespace Rocket.DAL.UoW
+{
+    /// <summary>
+    /// Представляет unit of work.
+    /// Содержит репозитории использующие один контекст данных
+    /// </summary>
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DbContext _dbContext;
+        private IDbFilmRepository _dbFilmRepository;
+        private IDbTVSeriesRepository _dbTVSeriesRepository;
+        private IDbMusicRepository _dbMusicRepository;
+        private bool disposedValue = false;
+
+        /// <summary>
+        /// Возвращает репозиторий для фильмов
+        /// </summary>
+        public IDbFilmRepository FilmRepository
+        {
+            get
+            {
+                if (this._dbFilmRepository == null)
+                {
+                    this._dbFilmRepository = new DbFilmRepository(this._dbContext);
+                }
+                return this._dbFilmRepository;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает репозиторий для сериалов
+        /// </summary>
+        public IDbTVSeriesRepository TVSeriesRepository
+        {
+            get
+            {
+                if (this._dbTVSeriesRepository == null)
+                {
+                    this._dbTVSeriesRepository = new DbTVSeriesRepository(this._dbContext);
+                }
+                return this._dbTVSeriesRepository;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает репозиторий для музыки
+        /// </summary>
+        public IDbMusicRepository MusicRepository
+        {
+            get
+            {
+                if (this._dbMusicRepository == null)
+                {
+                    this._dbMusicRepository = new DbMusicRepository(this._dbContext);
+                }
+                return this._dbMusicRepository;
+            }
+        }
+
+        /// <summary>
+        /// Создает новый экземпляр <see cref="UnitOfWork"/>
+        /// c заданным контекстом данных
+        /// </summary>
+        /// <param name="dbContext">Экземпляр контекста данных</param>
+        public UnitOfWork(DbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        /// <summary>
+		/// Сохраняет изменения в хранилище данных
+		/// </summary>
+        public void Save()
+        {
+            this._dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Освобождает управляемые ресурсы
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Освобождает управляемые ресурсы
+        /// </summary>
+        /// <param name="disposing">Указывает был ли уже вызван метода Dispose ранее</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    this._dbContext.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+    }
+}
