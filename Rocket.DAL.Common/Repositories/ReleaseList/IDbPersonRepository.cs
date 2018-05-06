f86c69827730be18a901da87c851785095670f87
VasiliHolub <vasiliygolub@yandex.ru>
2018-05-06 22:26:41 +0300

    * Редактировние

--- Rocket.DAL.Common/Repositories/ReleaseList/IDbPersonRepository.cs
+++ Rocket.DAL.Common/Repositories/ReleaseList/IDbPersonRepository.cs
@@ -1,11 +1,11 @@
-﻿//using Rocket.DAL.Common.DbModels.User.DbPerson;
+﻿using Rocket.DAL.Common.DbModels.User.DbPerson;
 
-//namespace Rocket.DAL.Common.Repositories.ReleaseList
-//{
-//    /// <summary>
-//    /// Представляет репозиторий для людей (актёров, режиссёров)
-//    /// </summary>
-//    public interface IDbPersonRepository :IRepository<DbPerson>
-//    {
-//    }
-//}
+namespace Rocket.DAL.Common.Repositories.ReleaseList
+{
+    /// <summary>
+    /// Представляет репозиторий для людей (актёров, режиссёров)
+    /// </summary>
+    public interface IDbPersonRepository : IRepository<DbPerson>
+    {
+    }
+}
