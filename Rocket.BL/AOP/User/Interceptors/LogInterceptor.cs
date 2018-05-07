79a3b1895da36115fb05ecc3e5cd4bd4fe9c15a7
VasiliHolub <vasiliygolub@yandex.ru>
2018-05-07 23:49:11 +0300

    + Фабрика сервиса

--- Rocket.BL/AOP/User/Interceptors/LogInterceptor.cs
+++ Rocket.BL/AOP/User/Interceptors/LogInterceptor.cs
@@ -1,8 +1,34 @@
 ﻿using NLog;
+using Castle.DynamicProxy;
 
 namespace Rocket.BL.AOP.User.Interceptors
 {
-    class LogInterceptor
+    /// <summary>
+    /// Перехватчик, который выполняется при всех запусках методов
+    /// сервиса 'UserManagementService'
+    /// </summary>
+    public class LogInterceptor : IInterceptor
     {
+        #region IInterceptor Members
+
+        public void Intercept(IInvocation invocation)
+
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+
+            logger.Info($"Method {invocation.Method.Name}");
+
+            foreach (var item in invocation.Arguments)
+            {
+                logger.Info($"type: {item.GetType().ToString()}, value: {item.ToString()}");
+            }
+
+            invocation.Proceed();
+
+            logger.Info($"Post process {invocation.Method.Name} method");
+        
+        }
+
+        #endregion
     }
 }
