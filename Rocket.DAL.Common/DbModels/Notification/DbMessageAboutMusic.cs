14c42858c65837fcae82efa80264cc39edefd349
Ilya Dvortsevoy <links309@gmail.com>
2018-05-09 13:12:40 +0300

    + DbMessageAboutMusic model added

---
+++ Rocket.DAL.Common/DbModels/Notification/DbMessageAboutMusic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Rocket.DAL.Common.DbModels.Notification
+{
+    /// <summary>
+    /// Описывает модель хранения данных о музыкальном релизе
+    /// </summary>
+    public class DbMessageAboutMusic
+    {
+        /// <summary>
+        /// Возвращает или задает идентификационный номер сообщения
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает идентификационный номер музыкального релиза
+        /// </summary>
+        public int MusicId { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает название музыкального релиза
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает дату выхода музыкального релиза
+        /// </summary>
+        public DateTime ReleaseDate { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает время создания сообщения
+        /// </summary>
+        public DateTime CreationTime { get; set; }
+    }
+}
