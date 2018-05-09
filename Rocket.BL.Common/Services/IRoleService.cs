ad20b2c3e48e4cf9fe301d0aaa78f0247307de42
medpavol <medpavol@gmail.com>
2018-05-09 18:41:48 +0300

    * IRoleService refactoring.

--- Rocket.BL.Common/Services/IRoleService.cs
+++ Rocket.BL.Common/Services/IRoleService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq.Expressions;
 using Rocket.BL.Common.Models.UserRoles;
 
 namespace Rocket.BL.Common.Services
@@ -8,22 +9,53 @@ namespace Rocket.BL.Common.Services
         /// <summary>
         /// Меняет значение IsActive для роли на противоположное
         /// </summary>
-        void SwitchRoleVisibility(Role role);
+        void SwitchRoleActivity(Role role);
 
 
-        // todo добавить роль по умолчанию в куда-нибудь
-        // private const IRole DefaultRole = null;
 
-        // public void ChangeUserRole(IUser user)
-        // {
-        //    ChangeUserRole(user, DefaultRole);
-        // }
+        /// <summary>
+        /// сетапим роль по умолчанию нашему юзеру
+        /// </summary>
+        void ChangeUserRole();
 
-        // public void ChangeUserRole(IUser user, IRole role)
-        // {
-        //    // todo сетапим роль нашему юзверю
-        //    // _user?.SetUserRole(_role);
-        // }
+        /// <summary>
+        ///  сетапим выбранную роль нашему юзверю
+        /// </summary>
+        /// <param name="role"></param>
+        void ChangeUserRole(Role role);
 
+        /// <summary>
+        /// возвращает Роль по Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Role GerRoleById(int id);
+
+        /// <summary>
+        /// Добавляет новую роль в хранилище данных
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        void AddNewRole(Role role);
+
+        /// <summary>
+        /// Обновляет существующую роль в хранилище
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        void UpdateRole(Role role);
+
+        /// <summary>
+        /// Удаляет роль по Id из хранилища данных
+        /// </summary>
+        /// <param name="id"></param>
+        void DeleteRole(int id);
+
+        /// <summary>
+        /// проверяем наличие в хранилище данных роли с указанным фильтом
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        bool RoleIsExists(Expression<Func<Role, bool>> filter);
     }
 }
--- Rocket.BL/Services/UserServices/UserRoleService.cs
+++ Rocket.BL/Services/UserServices/UserRoleService.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using AutoMapper;
 using Rocket.BL.Common.Models.UserRoles;
+using Rocket.BL.Common.Services;
 using Rocket.DAL.Common.DbModels.DbUserRole;
-using Rocket.DAL.Common.Repositories.IDbUserRoleRepository;
 using Rocket.DAL.Common.UoW;
 
 namespace Rocket.BL.Services.UserServices
@@ -14,32 +12,26 @@ namespace Rocket.BL.Services.UserServices
     /// получение роли, установка роли для пользователя
     /// если не указана, то дефолтовая
     /// </summary>
-    public class UserRoleService : BaseService, IDbRoleRepository //todo add ilogger
+    public class UserRoleService : BaseService, IRoleService //todo add ilogger
     {
-
 
         public UserRoleService(IUnitOfWork unitOfWork) : base(unitOfWork)
         {
 
         }
 
-        public IEnumerable<DbRole> Get(Expression<Func<DbRole, bool>> filter = null,  // ?2
-                Func<IQueryable<DbRole>, IOrderedQueryable<DbRole>> orderBy = null, string includeProperties = "")
+
+        public Role GerRoleById(int id)
         {
-            throw new NotImplementedException();
+            return Mapper.Map<Role>(
+                _unitOfWork.TVSeriesRepository.GetById(id));
         }
 
-        public DbRole GetById(int id)  // ?1
-        {
-            return _unitOfWork.RoleRepository.GetById(id);
-        }
-
-        public int AddRole(DbRole role)
+        public void AddNewRole(Role role)
         {
             var dbRole = Mapper.Map<DbRole>(role);
-            this._unitOfWork.RoleRepository.Insert(dbRole);
-            this._unitOfWork.Save();
-            return dbRole.Id;
+            _unitOfWork.RoleRepository.Insert(dbRole);
+            _unitOfWork.Save();
         }
 
         public void UpdateRole(Role role)
@@ -49,15 +41,36 @@ namespace Rocket.BL.Services.UserServices
             _unitOfWork.Save();
         }
 
-        public void Delete(int id)
+        public void DeleteRole(int id)
         {
             _unitOfWork.RoleRepository.Delete(id);
             _unitOfWork.Save();
         }
 
-        public void Delete(DbRole entity) // лишний.
+        public void SwitchRoleActivity(Role role)
         {
-            throw new NotImplementedException();
+            // todo
+            // _unitOfWork.RoleRepository.SwitchActivity(role);
         }
+
+        public bool RoleIsExists(Expression<Func<Role, bool>> filter)
+        {
+            // todo
+            return true;
+        }
+
+        private const Role DefaultRole = null;
+        public void ChangeUserRole()
+        {
+            // todo добавить роль по умолчанию в куда-нибудь
+            ChangeUserRole(DefaultRole);
+        }
+
+        public void ChangeUserRole(Role role)
+        {
+            // todo
+        }
+
+
     }
 }
