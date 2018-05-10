b062c3eda8aa2ccf97d782391bbc43ccea0b958e
VasiliHolub <vasiliygolub@yandex.ru>
2018-05-10 11:10:10 +0300

    * Доработка тестов

--- Rocket.BL.Tests/User/FakeData/FakeDbUsers.cs
+++ Rocket.BL.Tests/User/FakeData/FakeDbUsers.cs
@@ -21,7 +21,7 @@ namespace Rocket.BL.Tests.User.FakeData
         /// <summary>
         /// Возвращает коллекцию сгенерированных пользователей
         /// </summary>
-        public List<DbUser> UsersFaker { get; }
+        public List<DbUser> Users{ get; }
 
         /// <summary>
         /// Создает новый экземпляр сгенерированных данных о пользователях
@@ -52,7 +52,7 @@ namespace Rocket.BL.Tests.User.FakeData
                 .RuleFor(p => p.Login, f => { return isLoginNullOrEmpty ? string.Empty : f.Lorem.Letter(minLoginLenght); })
                 .RuleFor(p => p.Password, f => { return isPasswordNullOrEmpty ? string.Empty : f.Lorem.Letter(minPasswordLenght); });
 
-            this.UsersFaker = result.Generate(usersCount);
+            this.Users = result.Generate(usersCount);
         }
     }
 }
