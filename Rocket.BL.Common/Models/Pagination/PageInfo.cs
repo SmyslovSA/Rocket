4632d373cebe1a9155f04fe47c20c99b3343a464
Mikalai Kharevich <mikalaikharevich@gmail.com>
2018-05-10 20:30:44 +0300

    + добавлен обобщенный класс информации о странице элементов * в BaseRepository добавлен метод получения количества элементов в хранилище * в BaseRepository добавлен метод получения страницы в виде коллекции элементов

---
+++ Rocket.BL.Common/Models/Pagination/PageInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Rocket.BL.Common.Models.Pagination
+{
+    /// <summary>
+    /// Представляет модель постраничного просмотра коллекций
+    /// </summary>
+    /// <typeparam name="T">Тип элементов коллекции</typeparam>
+    public class PageInfo<T>
+    {
+        /// <summary>
+        /// Возвращает или задает номер страницы
+        /// </summary>
+        public int PageNumber { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает коллекцию элементов страницы
+        /// </summary>
+        public ICollection<T> PageItems { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает размер страницы
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает количество всех страниц
+        /// </summary>
+        public int TotalPagesCount { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает количество всех элементов
+        /// </summary>
+        public int TotalItemsCount { get; set; }
+    }
+}
--- Rocket.DAL.Common/Repositories/IBaseRepository.cs
+++ Rocket.DAL.Common/Repositories/IBaseRepository.cs
@@ -27,6 +27,25 @@ namespace Rocket.DAL.Common.Repositories
             string includeProperties = "");
 
         /// <summary>
+        /// Возвращает страницу заданного размера с заданным номером
+        /// в виде перечисления экземпляров <see cref="TEntity"/> из хранилища данных.
+        /// Применяет фильтр, сортировку и загрузку связанных свойств,
+        /// если заданы соответствующие значения параметров
+        /// </summary>
+        /// <param name="pageSize"></param>
+        /// <param name="pageNumber">Лямбда-выражение определяющее фильтрацию экземпляров <see cref="TEntity"/></param>
+        /// <param name="filter">Лямбда-выражение определяющее фильтрацию экземпляров <see cref="TEntity"/></param>
+        /// <param name="orderBy">Лямбда-выражение определяющее сортировку экземпляров <see cref="TEntity"/></param>
+        /// <param name="includeProperties">Список связанных свойств экземпляров <see cref="TEntity"/>, разделенный запятыми</param>
+        /// <returns>Перечисление экземпляров <see cref="TEntity"/></returns>
+        IEnumerable<TEntity> GetPage(
+            int pageSize,
+            int pageNumber,
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+
+        /// <summary>
         /// Возвращает экземпляр <see cref="TEntity"/>,
         /// соответствующий заданному идентификатору, из хранилища данных
         /// </summary>
@@ -58,6 +77,13 @@ namespace Rocket.DAL.Common.Repositories
         /// </summary>
         /// <param name="entity">Экземпляр <see cref="TEntity"/></param>
         void Delete(TEntity entity);
-    }
 
+        /// <summary>
+        /// Возвращает количество элементов в репозитории,
+        /// соответствующих заданному фильтру
+        /// </summary>
+        /// <param name="filter">Лямбда-выражение определяющее фильтрацию экземпляров <see cref="TEntity"/></param>
+        /// <returns>Количество элементов</returns>
+        int ItemsCount(Expression<Func<TEntity, bool>> filter = null);
+    }
 }
