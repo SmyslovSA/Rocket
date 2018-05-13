37a24902d685fe3c3c84e2aa91eec593db7093ea
medpavol <medpavol@gmail.com>
2018-05-13 21:05:59 +0300

    + web Api  initial

---
+++ Rocket.Web/Controllers/UserRole/RoleController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Rocket.Web.Controllers.UserRole
+{
+    public class RoleController : ApiController
+    {
+        [HttpGet]
+        public void GetAllRoles()
+        {
+
+        }
+
+        [HttpGet]
+        public void GetRoleById(int id)
+        {
+
+        }
+
+        [HttpPost]
+        public void SaveRole()
+        {
+
+        }
+
+        [HttpPut]
+        public void UpdateRole()
+        {
+
+        }
+
+        [HttpDelete]
+        public void DeleteRoleById()
+        {
+
+        }
+
+        [HttpPut]
+        public void SetDefaultRole()
+        {
+
+        }
+
+    }
+}
