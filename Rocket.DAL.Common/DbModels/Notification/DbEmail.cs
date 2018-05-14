9008d4197804af564dd1f6ee9b3b74340ecc2574
Ilya Dvortsevoy <links309@gmail.com>
2018-05-14 16:58:50 +0300

    + DbEmail model created

---
+++ Rocket.DAL.Common/DbModels/Notification/DbEmail.cs
@@ -0,0 +1,29 @@
+﻿namespace Rocket.DAL.Common.DbModels.Notification
+{
+    /// <summary>
+    /// Описывает модель хранения данных о email адресе получателя нотификации
+    /// </summary>
+    public class DbEmail
+    {
+        /// <summary>
+        /// Возвращает или задает идентификационный номер email адреса
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает получателя, кторому принадлежит email адрес
+        /// </summary>
+        public DbReceiver Receiver { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает идентификационный номер получателя
+        /// кторому принадлежит email адрес
+        /// </summary>
+        public int ReceiverId { get; set; }
+
+        /// <summary>
+        /// Возвращает или задает строковое представление email адреса
+        /// </summary>
+        public string EmailTitle { get; set; }
+    }
+}
