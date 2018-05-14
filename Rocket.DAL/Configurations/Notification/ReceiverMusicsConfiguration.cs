ce709d05862dfd1c0badcee8425173a314adefe1
Ilya Dvortsevoy <links309@gmail.com>
2018-05-14 22:25:37 +0300

    + ReceiverMusicsConfiguration file added

---
+++ Rocket.DAL/Configurations/Notification/ReceiverMusicsConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Rocket.DAL.Common.DbModels.Notification;
+
+namespace Rocket.DAL.Configurations.Notification
+{
+    /// <summary>
+    /// Конфигурация хранения сводных данных о получателе и музыкальном релизе
+    /// </summary>
+    public class ReceiverMusicsConfiguration: EntityTypeConfiguration<ReceiversJoinMusics>
+    {
+        public ReceiverMusicsConfiguration()
+        {
+            ToTable("ReceiversJoinMusics");
+
+            HasKey(x => new { x.ReceiverId, x.MusicMessageId });
+
+            Property(x => x.Viewed).IsRequired();
+        }
+    }
+}
