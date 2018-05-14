ed37d1a0f7751f8202010d372341cc2a1258e5ef
Ilya Dvortsevoy <links309@gmail.com>
2018-05-14 22:14:06 +0300

    + FilmMessageConfiguration file added

---
+++ Rocket.DAL/Configurations/Notification/FilmMessageConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Rocket.DAL.Common.DbModels.Notification;
+
+namespace Rocket.DAL.Configurations.Notification
+{
+    /// <summary>
+    /// Конфигурация хранения данных по сообщению о релизе фильма
+    /// </summary>
+    public class FilmMessageConfiguration: EntityTypeConfiguration<DbFilmMessage>
+    {
+        public FilmMessageConfiguration()
+        {
+            ToTable("FilmMessages");
+
+            HasKey(x => x.Id);
+
+            HasMany(x => x.ReceiversJoinFilms)
+                .WithRequired(x => x.FilmMessage)
+                .HasForeignKey(x => x.FilmMessageId);
+
+            Property(x => x.FilmId).IsRequired();
+
+            Property(x => x.Title).IsRequired();
+
+            Property(x => x.ReleaseDate).IsRequired();
+
+            Property(x => x.CreationTime).IsRequired();
+        }
+    }
+}
