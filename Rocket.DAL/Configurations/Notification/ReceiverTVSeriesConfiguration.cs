050a3c9e8317ed0380b7c4ee3a83d89e087fd20a
Ilya Dvortsevoy <links309@gmail.com>
2018-05-14 22:25:51 +0300

    + ReceiverTVSeriesConfiguration file added

---
+++ Rocket.DAL/Configurations/Notification/ReceiverTVSeriesConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Rocket.DAL.Common.DbModels.Notification;
+
+namespace Rocket.DAL.Configurations.Notification
+{
+    /// <summary>
+    /// Конфигурация хранения сводных данных о получателе и релизе сериала
+    /// </summary>
+    public class ReceiverTVSeriesConfiguration: EntityTypeConfiguration<ReceiversJoinTVSeries>
+    {
+        public ReceiverTVSeriesConfiguration()
+        {
+            ToTable("ReceiversJoinTVSeries");
+
+            HasKey(x => new { x.ReceiverId, x.TVSeriesMessageId });
+
+            Property(x => x.Viewed).IsRequired();
+        }
+    }
+}
