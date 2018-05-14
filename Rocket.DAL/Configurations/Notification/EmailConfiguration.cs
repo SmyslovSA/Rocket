5f730b29b380cde620897b93e32933056988cb0e
Ilya Dvortsevoy <links309@gmail.com>
2018-05-14 22:13:44 +0300

    + EmailConfiguration file added

---
+++ Rocket.DAL/Configurations/Notification/EmailConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Rocket.DAL.Common.DbModels.Notification;
+
+namespace Rocket.DAL.Configurations.Notification
+{
+    /// <summary>
+    /// Конфигурация хранения данных email адреса получателя
+    /// </summary>
+    public class EmailConfiguration: EntityTypeConfiguration<DbEmail>
+    {
+        public EmailConfiguration()
+        {
+            ToTable("Emails");
+
+            HasKey(x => x.Id);
+
+            Property(x => x.EmailTitle).IsRequired();
+        }
+    }
+}
