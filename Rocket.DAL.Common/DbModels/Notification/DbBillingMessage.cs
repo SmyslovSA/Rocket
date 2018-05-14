9fb0be78255c0636c1eb27a96fd53dfc2142edd0
Ilya Dvortsevoy <links309@gmail.com>
2018-05-14 16:30:51 +0300

    * DbBillingMessage model updated

--- Rocket.DAL.Common/DbModels/Notification/DbBillingMessage.cs
+++ Rocket.DAL.Common/DbModels/Notification/DbBillingMessage.cs
@@ -19,6 +19,11 @@ namespace Rocket.DAL.Common.DbModels.Notification
         public DbReceiver Receiver { get; set; }
 
         /// <summary>
+        /// Возвращает или задает идентификационный номер получателя сообщения
+        /// </summary>
+        public int ReceiverId { get; set; }
+
+        /// <summary>
         /// Возвращает или задает оплаченную сумму
         /// </summary>
         public decimal Sum { get; set; }
