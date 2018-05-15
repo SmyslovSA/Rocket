b74f0ad98f61e2df10120c38453e431e97a4cf3c
Sergey-PC\Sergey <alsergik86@gmail.com>
2018-05-15 15:46:35 +0300

    * Update Service

--- Rocket.BL/Services/UserServices/PermissionManagerService.cs
+++ Rocket.BL/Services/UserServices/PermissionManagerService.cs
@@ -1,28 +1,43 @@
-﻿using Rocket.BL.Common.Models.UserRoles;
+﻿using System.Security;
+using Rocket.BL.Common.Models.UserRoles;
 using Rocket.BL.Common.Services;
+using Rocket.BL.Services.ReleaseList;
+using Rocket.DAL.Common.DbModels.DbUserRole;
+using Rocket.DAL.Common.UoW;
+using AutoMapper;
 
 namespace Rocket.BL.Services.UserServices
 {
     /// <summary>
     /// Добавление/удаление пермишенов у ролей + логирование
     /// </summary>
-    public class PermissionManagerService : IPermissionService
+    public class PermissionManagerService : BaseService
     {
 
-
-        public PermissionManagerService() // todo add ilogger
+        /// <summary>
+        /// Создает новый экземпляр <see cref="FilmDetailedInfoService"/>
+        /// с заданным unit of work
+        /// </summary>
+        /// <param name="unitOfWork">Экземпляр unit of work</param>
+        public PermissionManagerService(IUnitOfWork unitOfWork)
+            : base(unitOfWork)
         {
-
         }
 
-        public void AddPermissionToRole()
+        public void AddPermissionToRole(int idRole, int idPermission)
         {
             // докидываем пермишен в роль
+            DbRole aRole =  Mapper.Map<DbRole>(_unitOfWork.RoleRepository.GetById(idRole));
+            DbPermission aPermission = Mapper.Map<DbPermission>(_unitOfWork.RoleRepository.GetById(idRole));
+            aRole.Permissions.Add(aPermission);
         }
 
-        public void RemovePermissionFromRole()
+        public void RemovePermissionFromRole(int idRole, int idPermission)
         {
             // удаляем пермишен у роли
+            DbRole aRole = Mapper.Map<DbRole>(_unitOfWork.RoleRepository.GetById(idRole));
+            DbPermission aPermission = Mapper.Map<DbPermission>(_unitOfWork.RoleRepository.GetById(idRole));
+            aRole.Permissions.Remove(aPermission);
         }
     }
 }
