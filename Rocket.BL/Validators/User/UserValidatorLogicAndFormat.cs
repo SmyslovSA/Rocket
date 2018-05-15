7ae778d9c343ddbaf7e2f0a81fad1e4e621e0ecc
VasiliHolub <vasiliygolub@yandex.ru>
2018-05-15 16:29:32 +0300

    * Форматирование

--- Rocket.BL/Validators/User/UserValidatorLogicAndFormat.cs
+++ Rocket.BL/Validators/User/UserValidatorLogicAndFormat.cs
@@ -16,29 +16,29 @@ namespace Rocket.BL.Validators.User
             string userAccountPasswordLenghtAssembleFullMessage = Resources.USERACCOUNTPASSWORDLENGHT +
                                                                   Resources.USERACCOUNTPASSWORDMINLENGHT +
                                                                   Resources
-                                                                      .USERACCOUNTPASSWORDLOGINPARTOFSTRINGFORCOMPOSITION;
+                                                                     .USERACCOUNTPASSWORDLOGINPARTOFSTRINGFORCOMPOSITION;
 
             // Содержит строку с сообщением и минимальным количеством символов,
             // которые должны быть в логине.
             string userAccountLoginLenghtAssembleFullMessage = Resources.USERACCOUNTPASSWORDLENGHT +
                                                                Resources.USERACCOUNTPASSWORDMINLENGHT +
                                                                Resources
-                                                                   .USERACCOUNTPASSWORDLOGINPARTOFSTRINGFORCOMPOSITION;
+                                                                  .USERACCOUNTPASSWORDLOGINPARTOFSTRINGFORCOMPOSITION;
 
             RuleFor(x => x.Login)
-                .NotEmpty()
-                .WithMessage(Resources.USERACCOUNTLOGINISEMPTY)
-                .NotNull()
-                .WithMessage(Resources.USERACCOUNTLOGINISEMPTY)
-                .MinimumLength(Convert.ToInt32(Resources.USERACCOUNTLOGINMINLENGTH))
-                .WithMessage(userAccountLoginLenghtAssembleFullMessage);
+               .NotEmpty()
+               .WithMessage(Resources.USERACCOUNTLOGINISEMPTY)
+               .NotNull()
+               .WithMessage(Resources.USERACCOUNTLOGINISEMPTY)
+               .MinimumLength(Convert.ToInt32(Resources.USERACCOUNTLOGINMINLENGTH))
+               .WithMessage(userAccountLoginLenghtAssembleFullMessage);
             RuleFor(x => x.Password)
-                .NotEmpty()
-                .WithMessage(Resources.USERACCOUNTPASSWORDISEMPTY)
-                .NotNull()
-                .WithMessage(Resources.USERACCOUNTPASSWORDISEMPTY)
-                .MinimumLength(Convert.ToInt32(Resources.USERACCOUNTPASSWORDMINLENGHT))
-                .WithMessage(userAccountPasswordLenghtAssembleFullMessage);
+               .NotEmpty()
+               .WithMessage(Resources.USERACCOUNTPASSWORDISEMPTY)
+               .NotNull()
+               .WithMessage(Resources.USERACCOUNTPASSWORDISEMPTY)
+               .MinimumLength(Convert.ToInt32(Resources.USERACCOUNTPASSWORDMINLENGHT))
+               .WithMessage(userAccountPasswordLenghtAssembleFullMessage);
         }
     }
 }
