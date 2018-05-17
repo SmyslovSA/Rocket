ed8e4d4ff8e6e4665635c85bddf679589ade6e13
Ilya Dvortsevoy <links309@gmail.com>
2018-05-17 19:07:03 +0300

    + DbCustomMessageRepository class added

---
+++ Rocket.DAL/Repositories/Notification/DbCustomMessageRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using Rocket.DAL.Common.DbModels.Notification;
+using Rocket.DAL.Common.Repositories.Notification;
+
+namespace Rocket.DAL.Repositories.Notification
+{
+    /// <summary>
+    /// Представляет репозиторий для сообщений произвольного содержания
+    /// </summary>
+    class DbCustomMessageRepository : BaseRepository<DbCustomMessage>, IDbCustomMessageRepository
+    {
+        /// <summary>
+        /// Создает новый экземпляр репозитория для сообщений произвольного содержания
+        /// с заданным контекстом базы данных
+        /// </summary>
+        /// <param name="dbContext">Экземпляр контекста базы данных</param>
+        public DbCustomMessageRepository(DbContext dbContext)
+            : base(dbContext)
+        {
+        }
+    }
+}
