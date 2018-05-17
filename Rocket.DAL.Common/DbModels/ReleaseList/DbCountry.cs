bc4901d1fcba0f1a9cb516ad7a6e3d31f50399a3
VasiliHolub <vasiliygolub@yandex.ru>
2018-05-17 16:02:46 +0300

    Revert "*"
    This reverts commit d159599c2e2202b6ea128b14dff717e73c43f54b.

--- Rocket.DAL.Common/DbModels/ReleaseList/DbCountry.cs
+++ Rocket.DAL.Common/DbModels/ReleaseList/DbCountry.cs
@@ -1,45 +1,32 @@
 ﻿using System.Collections.Generic;
-using Rocket.DAL.Common.DbModels.User;
 
 namespace Rocket.DAL.Common.DbModels.ReleaseList
 {
-    /// <summary>
-    /// Представляет модель хранения данных о странах
-    /// </summary>
-    public class DbCountry
-    {
-        /// <summary>
-        /// Возвращает или задает уникальный идентификатор страны
-        /// </summary>
-        public int Id { get; set; }
+    ///// <summary>
+    ///// Представляет модель хранения данных о странах
+    ///// </summary>
+    //public class DbCountry
+    //{
+    //    /// <summary>
+    //    /// Возвращает или задает уникальный идентификатор страны
+    //    /// </summary>
+    //    public int Id { get; set; }
 
-        /// <summary>
-        /// Возвращает или задает название страны
-        /// </summary>
-        public string Name { get; set; }
+    //    /// <summary>
+    //    /// Возвращает или задает название страны
+    //    /// </summary>
+    //    public string Name { get; set; }
 
-        /// <summary>
-        /// Возвращает или задает коллекцию фильмов,
-        /// в съёмках которых принимала участие эта страна.
-        /// </summary>
-        public ICollection<DbFilm> DbFilms { get; set; }
+    //    /// <summary>
+    //    /// Возвращает или задает коллекцию фильмов,
+    //    /// в съёмках которых принимала участие эта страна.
+    //    /// </summary>
+    //    public ICollection<DbFilm> DbFilms { get; set; }
 
-        /// <summary>
-        /// Возвращает или задает коллекцию сериалов,
-        /// в съёмках которых принимала участие эта страна
-        /// </summary>
-        public ICollection<DbTVSeries> DbTVSerials { get; set; }
-
-        /// <summary>
-        /// Возвращает или задает коллекцию детальной информации о пользователях,
-        /// гражданинами которой они являются.
-        /// </summary>
-        public ICollection<DbUserDetail> DbUserDetails { get; set; }
-
-        /// <summary>
-        /// Возвращает или задает коллекцию адресов детальной информации пользователей,
-        /// к которым относится данная страна.
-        /// </summary>
-        public ICollection<DbAddress> DbAddresses { get; set; }
-    }
+    //    /// <summary>
+    //    /// Возвращает или задает коллекцию сериалов,
+    //    /// в съёмках которых принимала участие эта страна
+    //    /// </summary>
+    //    public ICollection<DbTVSeries> DbTVSerials { get; set; }
+    //}
 }
