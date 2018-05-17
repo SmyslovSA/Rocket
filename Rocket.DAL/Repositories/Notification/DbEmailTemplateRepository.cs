c6c3bb937ca68214f92fc81101f322f29f3a5c57
Ilya Dvortsevoy <links309@gmail.com>
2018-05-18 00:54:57 +0300

    * DbEmailTemplateRepository class updated

--- Rocket.DAL/Repositories/Notification/DbEmailTemplateRepository.cs
+++ Rocket.DAL/Repositories/Notification/DbEmailTemplateRepository.cs
@@ -1,13 +1,22 @@
-﻿using Rocket.DAL.Common.DbModels.Notification;
+﻿using System.Data.Entity;
+using Rocket.DAL.Common.DbModels.Notification;
 using Rocket.DAL.Common.Repositories.Notification;
 
 namespace Rocket.DAL.Repositories.Notification
 {
     /// <summary>
-    /// Представляет репозиторий для сообщений произвольного содержания
+    /// Представляет репозиторий для шаблонов email сообщений
     /// </summary>
     public class DbEmailTemplateRepository: BaseRepository<DbEmailTemplate>, IDbEmailTemplateRepository
     {
-
+        /// <summary>
+        /// Создает новый экземпляр репозитория для шаблонов email сообщений
+        /// с заданным контекстом базы данных
+        /// </summary>
+        /// <param name="dbContext">Экземпляр контекста базы данных</param>
+        public DbEmailTemplateRepository(DbContext dbContext)
+            : base(dbContext)
+        {
+        }
     }
 }
