a62b826fc30394b3b4d459525beee1bed508612e
Ilya Dvortsevoy <links309@gmail.com>
2018-05-17 17:39:48 +0300

    + IDbReceiverRepository interface added

---
+++ Rocket.DAL.Common/Repositories/Notification/IDbReceiverRepository.cs
@@ -0,0 +1,11 @@
+﻿using Rocket.DAL.Common.DbModels.Notification;
+
+namespace Rocket.DAL.Common.Repositories.Notification
+{
+    /// <summary>
+    /// Представляет интерфейс репозитория для пользователей, являющихся получателями сообщений
+    /// </summary>
+    interface IDbReceiverRepository : IBaseRepository<DbReceiver>
+    {
+    }
+}
