65744661b1325d83fe7ce644271c1de9a550ddd3
Ilya Dvortsevoy <links309@gmail.com>
2018-05-18 12:41:40 +0300

    + DbReleaseMessageRepository class added

---
+++ Rocket.DAL/Repositories/Notification/DbReleaseMessageRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using Rocket.DAL.Common.DbModels.Notification;
+using Rocket.DAL.Common.Repositories.Notification;
+
+namespace Rocket.DAL.Repositories.Notification
+{
+    /// <summary>
+    /// Представляет репозиторий для сообщений о релизе
+    /// </summary>
+    public class DbReleaseMessageRepository : BaseRepository<DbReleaseMessage>, IDbReleaseMessageRepository
+    {
+        /// <summary>
+        /// Создает новый экземпляр репозитория для сообщений о релизе
+        /// с заданным контекстом базы данных
+        /// </summary>
+        /// <param name="dbContext">Экземпляр контекста базы данных</param>
+        public DbReleaseMessageRepository(DbContext dbContext)
+            : base(dbContext)
+        {
+        }
+    }
+}
