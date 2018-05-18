9a0c195a816787981f1f5a1e880b715e0d415068
Ilya Dvortsevoy <links309@gmail.com>
2018-05-18 12:29:47 +0300

    + DbGuestBillingMessageRepository class added

---
+++ Rocket.DAL/Repositories/Notification/DbGuestBillingMessageRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using Rocket.DAL.Common.DbModels.Notification;
+using Rocket.DAL.Common.Repositories.Notification;
+
+namespace Rocket.DAL.Repositories.Notification
+{
+    /// <summary>
+    /// Представляет репозиторий для сообщений с информацией о совершенном гостем донате
+    /// </summary>
+    class DbGuestBillingMessageRepository : BaseRepository<DbGuestBillingMessage>, IDbGuestBillingMessageRepository
+    {
+        /// <summary>
+        /// Создает новый экземпляр репозитория для сообщений с информацией о совершенном гостем донате
+        /// с заданным контекстом базы данных
+        /// </summary>
+        /// <param name="dbContext">Экземпляр контекста базы данных</param>
+        public DbGuestBillingMessageRepository(DbContext dbContext)
+            : base(dbContext)
+        {
+        }
+    }
+}
