a1baaef0abfb2639ca25ced4077f9afb70c9e008
VasiliHolub <vasiliygolub@yandex.ru>
2018-05-19 16:15:12 +0300

    * Закомментировал изменения в контексте

--- Rocket.DAL/Context/RocketContext.cs
+++ Rocket.DAL/Context/RocketContext.cs
@@ -94,15 +94,16 @@ namespace Rocket.DAL.Context
             modelBuilder.Configurations.Add(new SeasonEntityMap());
 
             // Группа домена пользователя.
-            modelBuilder.Configurations.Add(new DbAccountLevelConfiguration());
-            modelBuilder.Configurations.Add(new DbAccountLevelConfiguration());
-            modelBuilder.Configurations.Add(new DbAddressConfiguration());
-            modelBuilder.Configurations.Add(new DbEmailAddressConfiguratin());
-            modelBuilder.Configurations.Add(new DbGenderConfiguration());
-            modelBuilder.Configurations.Add(new DbHowToCallConfiguration());
-            modelBuilder.Configurations.Add(new DbLanguageConfiguration());
-            modelBuilder.Configurations.Add(new DbPhoneNumberConfiguration());
-            modelBuilder.Configurations.Add(new DbUserConfiguration());
-            modelBuilder.Configurations.Add(new DbUserDetailConfiguration());
+            //modelBuilder.Configurations.Add(new DbAccountLevelConfiguration());
+            //modelBuilder.Configurations.Add(new DbAccountLevelConfiguration());
+            //modelBuilder.Configurations.Add(new DbAddressConfiguration());
+            //modelBuilder.Configurations.Add(new DbEmailAddressConfiguratin());
+            //modelBuilder.Configurations.Add(new DbGenderConfiguration());
+            //modelBuilder.Configurations.Add(new DbHowToCallConfiguration());
+            //modelBuilder.Configurations.Add(new DbLanguageConfiguration());
+            //modelBuilder.Configurations.Add(new DbPhoneNumberConfiguration());
+            //modelBuilder.Configurations.Add(new DbUserConfiguration());
+            //modelBuilder.Configurations.Add(new DbUserDetailConfiguration());
+        }
     }
 }
