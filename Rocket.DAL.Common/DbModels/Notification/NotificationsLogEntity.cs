b8f11b9276eff506dbfe0d40c885c5dd6e07546e
Denis Susha <lumen446@gmail.com>
2018-05-20 16:19:33 +0300

    *upd Rocket.Notifications

---
+++ Rocket.DAL.Common/DbModels/Notification/NotificationsLogEntity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Rocket.DAL.Common.Enums;
+
+namespace Rocket.DAL.Common.DbModels.Notification
+{
+    /// <summary>
+    /// Лог нотификации
+    /// </summary>
+    public class NotificationsLogEntity
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Тип уведомления
+        /// </summary>
+        public NotificationType NotificationType { get; set; }
+
+        /// <summary>
+        /// Id пользователя
+        /// </summary>
+        public int UserId { get; set; }
+        
+        /// <summary>
+        /// Тип релиза
+        /// </summary>
+        public ReleaseType ReleaseType { get; set; }
+
+        /// <summary>
+        /// Id релиза
+        /// </summary>
+        public int ReleaseId { get; set; }
+
+        /// <summary>
+        /// Дата и время создания
+        /// </summary>
+        public DateTime CreatedDateTime { get; private set;}
+    }
+}
