117f502ead16c91326ced1585f801503ea538c8e
medpavol <medpavol@gmail.com>
2018-05-20 20:55:41 +0300

    * cop style next

--- Rocket.BL.Common/Services/IRoleService.cs
+++ Rocket.BL.Common/Services/IRoleService.cs
@@ -12,15 +12,15 @@ namespace Rocket.BL.Common.Services
         /// <summary>
         /// Удаляем модель по Id
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id"> Идентификатор </param>
         void Delete(int id);
 
         /// <summary>
         /// Получаем список ролей с фильтрами и сортировкой
         /// </summary>
-        /// <param name="filter"></param>
-        /// <param name="orderBy"></param>
-        /// <param name="includeProperties"></param>
+        /// <param name="filter"> фильтр </param>
+        /// <param name="orderBy"> слортировка </param>
+        /// <param name="includeProperties"> доп проперти </param>
         /// <returns> list </returns>
         IEnumerable<Role> Get(
             Expression<Func<DbRole, bool>> filter = null, 
@@ -30,27 +30,27 @@ namespace Rocket.BL.Common.Services
         /// <summary>
         /// Получаем роль по Id
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id"> Идентификатор </param>
         /// <returns>Role</returns>
         Role GetById(int id);
 
         /// <summary>
         /// Добавляем новую роль
         /// </summary>
-        /// <param name="role"></param>
+        /// <param name="role"> Роль </param>
         void Insert(Role role);
 
         /// <summary>
         /// Проверка существования данной роли
         /// </summary>
-        /// <param name="filter"></param>
+        /// <param name="filter"> фильтр </param>
         /// <returns></returns>
         bool RoleIsExists(Expression<Func<Role, bool>> filter);
 
         /// <summary>
         /// Обновляем текущую роль
         /// </summary>
-        /// <param name="role"></param>
+        /// <param name="role"> Роль </param>
         void Update(Role role);
     }
 }
