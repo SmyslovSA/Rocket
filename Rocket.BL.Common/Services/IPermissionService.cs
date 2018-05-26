91a5f66e9c432d46898a249a7dac9c4bdda218e6
medpavol <medpavol@gmail.com>
2018-05-26 09:36:27 +0300

    * cop style

--- Rocket.BL.Common/Services/IPermissionService.cs
+++ Rocket.BL.Common/Services/IPermissionService.cs
@@ -5,15 +5,15 @@
         /// <summary>
         /// Добавить существующую функц возможность для выбранной роли
         /// </summary>
-        /// <param name="idRole"></param>
-        /// <param name="idPermission"></param>
+        /// <param name="idRole"> ID role </param>
+        /// <param name="idPermission"> ID permission </param>
         void AddPermissionToRole(int idRole, int idPermission);
 
         /// <summary>
         /// Удалить функц возможность из текущего списка у роли
         /// </summary>
-        /// <param name="idRole"></param>
-        /// <param name="idPermission"></param>
+        /// <param name="idRole"> ID role </param>
+        /// <param name="idPermission"> ID permission </param>
         void RemovePermissionFromRole(int idRole, int idPermission);
     }
 }
