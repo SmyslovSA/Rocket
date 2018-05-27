17c4f1ae3bf6a3fc52d445fa45a4fe59152a614a
Goryachko Nickolay <mrappend@gmail.com>
2018-05-28 00:14:51 +0300

    + добавлен контроллер обработчика моментальных уведомлений с PayPal

---
+++ Rocket.Web/Controllers/UserPayments/IPNController.cs
@@ -0,0 +1,117 @@
+﻿using Rocket.BL.Common.Services;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rocket.Web.Controllers
+{
+    public class IPNController : Controller
+    {
+        private class IPNContext
+        {
+            public HttpRequestBase IPNRequest { get; set; }
+
+            public string RequestBody { get; set; }
+
+            public string Verification { get; set; } = String.Empty;
+        }
+
+        private readonly IUserPaymentService _userPaymentService;
+
+        public IPNController(IUserPaymentService userPaymentService)
+        {
+            _userPaymentService = userPaymentService;
+        }
+
+        public ActionResult Receive()
+        {
+            IPNContext ipnContext = new IPNContext()
+            {
+                IPNRequest = Request
+            };
+
+            using (StreamReader reader = new StreamReader(ipnContext.IPNRequest.InputStream, Encoding.ASCII))
+            {
+                ipnContext.RequestBody = reader.ReadToEnd();
+            }
+
+            //Store the IPN received from PayPal
+            LogRequest(ipnContext);
+
+            //Fire and forget verification task
+            Task.Run(() => VerifyTask(ipnContext));
+
+            //Reply back a 200 code
+            return new HttpStatusCodeResult(200);
+        }
+
+        private void VerifyTask(IPNContext ipnContext)
+        {
+            try
+            {
+                var verificationRequest = WebRequest.Create("https://www.sandbox.paypal.com/cgi-bin/webscr");
+
+                //Set values for the verification request
+                verificationRequest.Method = "POST";
+                verificationRequest.ContentType = "application/x-www-form-urlencoded";
+
+                //Add cmd=_notify-validate to the payload
+                string strRequest = "cmd=_notify-validate&" + ipnContext.RequestBody;
+                verificationRequest.ContentLength = strRequest.Length;
+
+                //Attach payload to the verification request
+                using (StreamWriter writer = new StreamWriter(verificationRequest.GetRequestStream(), Encoding.ASCII))
+                {
+                    writer.Write(strRequest);
+                }
+
+                //Send the request to PayPal and get the response
+                using (StreamReader reader = new StreamReader(verificationRequest.GetResponse().GetResponseStream()))
+                {
+                    ipnContext.Verification = reader.ReadToEnd();
+                }
+            }
+            catch (Exception exception)
+            {
+                //Capture exception for manual investigation
+            }
+
+            ProcessVerificationResponse(ipnContext);
+        }
+
+
+        private void LogRequest(IPNContext ipnContext)
+        {
+            // Persist the request values into a database or temporary data store
+        }
+
+        private void ProcessVerificationResponse(IPNContext ipnContext)
+        {
+            if (ipnContext.Verification.Equals("VERIFIED"))
+            {
+                // check that Payment_status=Completed
+                // check that Txn_id has not been previously processed
+                // check that Receiver_email is your Primary PayPal email
+                // check that Payment_amount/Payment_currency are correct
+                // process payment
+                var paymentInfo = ipnContext.RequestBody;
+                var payment = new BL.Common.Models.UserPayment();
+                //TODO:  parse info and write to payment
+                var user = new Rocket.BL.Common.Models.User.User();
+                _userPaymentService.AddUserPayment(payment);
+            }
+            else if (ipnContext.Verification.Equals("INVALID"))
+            {
+                //Log for manual investigation
+            }
+            else
+            {
+                //Log error
+            }
+        }
+    }
+}
