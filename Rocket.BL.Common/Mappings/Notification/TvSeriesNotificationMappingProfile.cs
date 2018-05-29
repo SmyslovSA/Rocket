0d22b1f6ef987252fa1360cd2c9a5b3e2da37139
Ilya Dvortsevoy <links309@gmail.com>
2018-05-29 16:01:35 +0300

    + mappings for notification added

---
+++ Rocket.BL.Common/Mappings/Notification/TvSeriesNotificationMappingProfile.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using AutoMapper;
+using Rocket.BL.Common.Models.Notification;
+using Rocket.DAL.Common.DbModels.Parser;
+using Rocket.DAL.Common.DbModels.User;
+
+namespace Rocket.BL.Common.Mappings.Notification
+{
+    class TvSeriesNotificationMappingProfile : Profile
+    {
+        public TvSeriesNotificationMappingProfile()
+        {
+            CreateMap<DbUser, Receiver>()
+                .ForMember(d => d.FirstName, opt => opt.MapFrom(s =>
+                    s.FirstName))
+                .ForMember(d => d.LastName, opt => opt.MapFrom(s =>
+                    s.LastName))
+                .ForMember(d => d.Emails, opt => opt.MapFrom(s =>
+                    s.DbAuthorisedUser.Email.Select(x => x.Name).ToList()));
+            CreateMap<TvSeriasEntity, TvSeriesNotification>()
+                .ForMember(d => d.Receivers, opt => opt.MapFrom(s =>
+                    s.Users))
+                .ForMember(d => d.Title, opt => opt.MapFrom(s =>
+                    s.TitleRu))
+                .ForMember(d => d.SeasonNumber, opt => opt.MapFrom(s =>
+                    s.ListSeasons.Last().Number))
+                .ForMember(d => d.EpisodeNumber, opt => opt.MapFrom(s =>
+                    s.ListSeasons.Last().ListEpisode.Last().Number))
+                .ForMember(d => d.ReleaseDate, opt => opt.MapFrom(s =>
+                    s.ListSeasons.Last().ListEpisode.Last().ReleaseDateRu));
+        }
+    }
+}
