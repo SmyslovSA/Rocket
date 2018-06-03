e05f834d4690ef764e6af122cd72969f6bb1c180
stepan.smyslov <stepan.smyslov@gmail.com>
2018-06-03 20:13:36 +0300

    * change user data/ password service

--- RocketFront/src/app/components/personal-area/personal-area.component.ts
+++ RocketFront/src/app/components/personal-area/personal-area.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {SimpleUser} from '../../models/personal-area/simpleuser';
 import {HttpClientModule} from '@angular/common/http';
 import { DataService } from '../../services/simpleuser.data.service';
-import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-personal-area',
@@ -14,15 +13,12 @@ export class PersonalAreaComponent implements OnInit {
   data: SimpleUser;
   constructor(private dataservice: DataService) {
     this.data = new SimpleUser();
-    this.data.FirstName = '';
-    this.data.LastName = '';
-    this.data.Avatar = '';
   }
   changeUserInfo() {
-    this.dataservice.changeData(this.data).subscribe(data => this.data = data);
+    this.dataservice.changeData(this.data).subscribe();
   }
   changeUserPassword(password: string, passwordConfirm: string) {
-    this.dataservice.changePassword(password, passwordConfirm).subscribe(data => this.data = data);
+    this.dataservice.changePassword(password, passwordConfirm).subscribe();
   }
   ngOnInit() {
     this.dataservice.getData().subscribe(data => this.data = data);
