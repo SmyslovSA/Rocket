952a5efad52e2289a100884d57a96772522c80ff
Ilya Dvortsevoy <links309@gmail.com>
2018-06-04 14:17:14 +0300

    * notification mappings updated

--- Rocket.BL.Common/Mappings/Notification/MusicNotificationMappingProfile.cs
+++ Rocket.BL.Common/Mappings/Notification/MusicNotificationMappingProfile.cs
@@ -1,7 +1,10 @@
 ﻿using System.Linq;
 using AutoMapper;
 using Rocket.BL.Common.Models.Notification;
+using Rocket.BL.Common.Models.ReleaseList;
+using Rocket.BL.Common.Models.Subscription;
 using Rocket.DAL.Common.DbModels.ReleaseList;
+using Rocket.DAL.Common.DbModels.Subscription;
 using Rocket.DAL.Common.DbModels.User;
 
 namespace Rocket.BL.Common.Mappings.Notification
@@ -11,15 +14,27 @@ namespace Rocket.BL.Common.Mappings.Notification
         public MusicNotificationMappingProfile()
         {
             CreateMap<DbUser, Receiver>()
-                .ForMember(d => d.FirstName, opt => opt.MapFrom(s => s.FirstName))
-                .ForMember(d => d.LastName, opt => opt.MapFrom(s => s.LastName))
-                .ForMember(d => d.Emails, opt => opt.MapFrom(s => s.DbAuthorisedUser.Email.Select(x => x.Name).ToList()));
+                .ForMember(d => d.FirstName, opt => opt.MapFrom(s =>
+                    s.FirstName))
+                .ForMember(d => d.LastName, opt => opt.MapFrom(s =>
+                    s.LastName))
+                .ForPath(d => d.Emails, opt => opt.MapFrom(s =>
+                    s.DbAuthorisedUser.Email.Select(x => x.Name).ToList()));
+
+            CreateMap<Musician, DbMusician>()
+                .IncludeBase<Subscribable, SubscribableEntity>()
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FullName))
+                .ReverseMap();
 
             CreateMap<DbMusic, MusicNotification>()
-                .ForMember(d => d.Receivers, opt => opt.MapFrom(s => s.Users))
-                .ForMember(d => d.Title, opt => opt.MapFrom(s => s.Title))
-                .ForMember(d => d.ReleaseDate, opt => opt.MapFrom(s => s.ReleaseDate))
-                .ForMember(d => d.Musicians, opt => opt.MapFrom(s => s.Musicians));
+                .ForMember(d => d.Receivers, opt => opt.MapFrom(s =>
+                    s.Users))
+                .ForMember(d => d.Title, opt => opt.MapFrom(s =>
+                    s.Title))
+                .ForMember(d => d.ReleaseDate, opt => opt.MapFrom(s =>
+                    s.ReleaseDate))
+                .ForMember(d => d.Musicians, opt => opt.MapFrom(s =>
+                    s.Musicians.ToList()));
         }
     }
 }
