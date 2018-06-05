7b1213cf77d69c2b25096b9ebb66885f57611be1
GOLUBVI\Admin <vasiliygolub@yandex.ru>
2018-06-05 16:52:35 +0300

    * Корректировка после мерджа

--- Rocket.BL.Common/Services/UserPayment/IUserPaymentService.cs
+++ Rocket.BL.Common/Services/UserPayment/IUserPaymentService.cs
@@ -1,4 +1,7 @@
-﻿namespace Rocket.BL.Common.Services.UserPayment
+﻿using Rocket.BL.Common.Models;
+using System;
+
+namespace Rocket.BL.Common.Services
 {
     /// <summary>
     /// Представляет сервис для валидации сведений о пользователе
@@ -11,12 +14,12 @@
         /// </summary>
         /// <param name="user">Экземпляр пользователя, для которого ищем инфу о платеже.</param>
         /// <returns>Платеж пользователя.</returns>
-        Models.UserPayment.UserPayment GetUserPayment(Models.User.User user);
+        UserPayment GetUserPayment(Models.User.User user);
 
         /// <summary>
         /// добавление инфы о платеже.
         /// </summary>
         /// <param name="payment">Информация о платеже</param>
-        void AddUserPayment(Models.UserPayment.UserPayment payment);
+        void AddUserPayment(UserPayment payment);
     }
 }
