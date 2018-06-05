e2d61a87e583d0a8ddb87a22a70de1690a133070
GOLUBVI\Admin <vasiliygolub@yandex.ru>
2018-06-05 16:57:00 +0300

    * Мерж

--- Rocket.BL/Services/UserPaymentService/UserPaymentService.cs
+++ Rocket.BL/Services/UserPaymentService/UserPaymentService.cs
@@ -1,14 +1,14 @@
 ﻿using AutoMapper;
-using Rocket.BL.Common.Models.UserPayment;
+using Rocket.BL.Common.Models;
 using Rocket.DAL.Common.DbModels;
 using Rocket.DAL.Common.UoW;
 
-namespace Rocket.BL.Services.UserPaymentService
+namespace Rocket.BL.Services
 {
     /// <summary>
     /// Представляет сервис для работы с платежами
     /// </summary>
-    public class UserPaymentService : BaseService, Common.Services.UserPayment.IUserPaymentService
+    public class UserPaymentService : BaseService, Common.Services.IUserPaymentService
     {
         public UserPaymentService(IUnitOfWork unitOfWork) : base(unitOfWork)
         {
