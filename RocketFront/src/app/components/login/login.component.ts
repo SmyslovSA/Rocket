c99ae22cfc131e402d6fece5057ab8376ae22bbb
Сычёв Никита <sychounm@gmail.com>
2018-06-06 20:38:50 +0300

    login

--- RocketFront/src/app/components/login/login.component.ts
+++ RocketFront/src/app/components/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { RocketAuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  userLogin: string;
+  password: string;
 
-  ngOnInit() {
+  constructor(private oauthService: OAuthService,
+    private rocketAuthService: RocketAuthService) {
   }
 
+  public login() {
+      this.rocketAuthService.login(this.userLogin, this.password);
+  }
+
+  public logoff() {
+      this.oauthService.logOut();
+  }
+  get givenName() {
+    const claims = this.oauthService.getIdentityClaims();
+    if (!claims) {
+      return null;
+    }
+    return claims['name'];
+  }
 }
